ec44a58a3924164bc13b684f73113ebca4efe132
Prasad <vavilala.prasad@gmail.com>
2019-10-12 12:36:37 -0400

    kafka-producer spring

--- kafka-producer-example/src/main/java/com/example/prasad/kafka/producer/App.java
+++ kafka-producer-example/src/main/java/com/example/prasad/kafka/producer/App.java
@@ -1,13 +1,47 @@
 package com.example.prasad.kafka.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! - Producer");
+		SpringApplication.run(App.class, args);
+
+		app.sendMessage("Topic_1", "Message");
+
+	}
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public void sendMessage(String topicName, String message) {
+
+		ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
+
+		future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+			@Override
+			public void onSuccess(SendResult<String, String> result) {
+				System.out.println(
+						"Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+			}
+
+			@Override
+			public void onFailure(Throwable ex) {
+				System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+			}
+		});
+	}
 }
